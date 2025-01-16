2a5c4b9f616e3d5ce8b046133212e2a319b1e6f8
DogukanBahsi <dgkn.bhsi@gmail.com>
2025-01-16 22:20:44 +0300

    Add files via upload

---
+++ Nesne Ödevi-1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+/*
+class Program
+{
+    static void Main()
+    {
+        int sayi = 10;
+        int kalan = sayi % 3;
+        Console.WriteLine("10 sayısının 3'e bölümünden kalan: " + kalan);
+    }
+}
+*/
+
+/*
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Birinci sayıyı girin: ");
+        double sayi1 = double.Parse(Console.ReadLine());
+        Console.Write("İkinci sayıyı girin: ");
+        double sayi2 = double.Parse(Console.ReadLine());
+
+        if (sayi2 != 0)
+        {
+            double bolum = sayi1 / sayi2;
+            Console.WriteLine("İki sayının bölümü: " + bolum);
+        }
+        else
+        {
+            Console.WriteLine("Bir sayıyı sıfıra bölemezsiniz.");
+        }
+    }
+}
+*/
+
+/*
+
+
+class Program
+{
+    static void Main()
+    {
+        int toplam = 0;
+        int carpim = 1;
+
+        for (int i = 1; i <= 4; i++)
+        {
+            Console.Write(i + ". sayıyı girin: ");
+            int sayi = int.Parse(Console.ReadLine());
+            toplam += sayi;
+            carpim *= sayi;
+        }
+
+        Console.WriteLine("Toplam: " + toplam);
+        Console.WriteLine("Çarpım: " + carpim);
+    }
+}
+*/
+
+/*
+
+class Program
+{
+    static void Main()
+    {
+        int sayi = 5;
+        int kare = sayi * sayi;
+        Console.WriteLine("5 sayısının karesi: " + kare);
+    }
+}
+*/
+
+/*
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Dikdörtgenler prizmasının uzunluğunu girin: ");
+        double uzunluk = double.Parse(Console.ReadLine());
+        Console.Write("Dikdörtgenler prizmasının genişliğini girin: ");
+        double genislik = double.Parse(Console.ReadLine());
+        Console.Write("Dikdörtgenler prizmasının yüksekliğini girin: ");
+        double yukseklik = double.Parse(Console.ReadLine());
+
+        double hacim = uzunluk * genislik * yukseklik;
+        Console.WriteLine("Dikdörtgenler prizmasının hacmi: " + hacim);
+    }
+}
+
+*/
+
+
+/*
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Birinci sayıyı girin: ");
+        int sayi1 = int.Parse(Console.ReadLine());
+        Console.Write("İkinci sayıyı girin: ");
+        int sayi2 = int.Parse(Console.ReadLine());
+
+        int fark = sayi1 - sayi2;
+        Console.WriteLine("İki sayının farkı: " + fark);
+    }
+}
+*/
